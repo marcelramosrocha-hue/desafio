42e439f0222588cfed2ff883033f63c4dd278fb5
marcelramosrocha-hue <marcelramosrocha@gmail.com>
2025-12-14 09:43:12 -0300

    Classificador de nivel de heroi

--- desafio/index.js
+++ desafio/index.js
@@ -1,1 +1,21 @@
-agddjfk;
+let nomeDoHeroi = "Ursão";
+let experienciaDoHeroi = 7900;
+
+
+if (experienciaDoHeroi <= 1000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Ferro " );
+} else if (experienciaDoHeroi <= 2000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Bronze " );
+} else if (experienciaDoHeroi <= 5000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Prata " );
+} else if (experienciaDoHeroi <= 7000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Ouro " );
+} else if (experienciaDoHeroi <= 8000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Platina " );
+} else if (experienciaDoHeroi <= 9000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Ascendente " );
+} else if (experienciaDoHeroi <= 10000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Imortal " );
+} else {
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Radiante " );
+}
